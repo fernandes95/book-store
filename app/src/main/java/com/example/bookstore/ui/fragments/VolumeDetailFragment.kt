796c960c6192f2d26c30951efc11d0d827e70b96
Tiago Fernandes <tiago_c_fernandes@hotmail.com>
2022-08-23 15:47:16 +0100

    Book Detail layout improvement
    -Changed favorite button to toolbar

--- app/src/main/java/com/example/bookstore/ui/fragments/VolumeDetailFragment.kt
+++ app/src/main/java/com/example/bookstore/ui/fragments/VolumeDetailFragment.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.Html
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
