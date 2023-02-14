0a1493b2e6d04b180593d570c24ca8a5db40556d
Tiago Fernandes <tiago_c_fernandes@hotmail.com>
2023-02-14 17:36:54 +0000

    Removed old code

--- app/src/main/java/com/example/bookstore/MainActivity.kt
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -6,44 +6,6 @@ import androidx.activity.compose.setContent
 import com.example.bookstore.ui.screens.VolumesApp
 import com.example.bookstore.ui.screens.theme.VolumesTheme
 
-/*class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var navView : BottomNavigationView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-
-        navView = binding.root.findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_other, R.id.VolumesFragment, R.id.favoritesFragment
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
-
-    fun showNavBar(show : Boolean){
-        navView.visibility = if(show) View.VISIBLE else View.GONE
-    }
-}*/
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
